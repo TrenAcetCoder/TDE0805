85ec017f58a92b492556e4a151a41b5ce61d6abf
Fernando Piana Pereira <fernando.pereira@grupointegrado.br>
2023-05-10 20:02:20 -0300

    peido2

--- PeidoJs.js
+++ PeidoJs.js
@@ -40,7 +40,6 @@ const express = require('express');
 
 const app = express();
 
-// Definir o recurso GET /health
 app.get('/health', (req, res) => {
   res.send('server is running');
 });
@@ -60,4 +59,88 @@ if (process.env.NODE_ENV !== 'production') {
 // Iniciar a aplicação em modo de produção
 app.listen(PORT, () => {
   console.log(`Aplicação escutando na porta ${PORT}`);
+});
+
+
+
+
+/* TDE 10/05/2023*/
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const tasks = [
+  {
+    id: 1,
+    name: 'Comprar leite',
+    description: 'Ir no mercado da esquina e comprar leite',
+    isDone: false,
+  },
+  {
+    id: 2,
+    name: 'Fazer exercícios',
+    description: 'Fazer exercícios físicos por 30 minutos',
+    isDone: true,
+  },
+  {
+    id: 3,
+    name: 'Estudar programação',
+    description: 'Estudar programação por 1 hora',
+    isDone: false,
+  },
+];
+
+let nextTaskId = 4;
+
+// GET /tasks: exibe a lista de tarefas cadastradas.
+app.get('/tasks', (req, res) => {
+  res.json(tasks);
+});
+
+// POST /tasks: cria uma nova tarefa.
+app.post('/tasks', (req, res) => {
+  const newTask = req.body;
+  newTask.id = nextTaskId;
+  nextTaskId++;
+  tasks.push(newTask);
+  res.json(newTask);
+});
+
+// PUT /tasks/:id: atualiza uma tarefa existente.
+app.put('/tasks/:id', (req, res) => {
+  const taskId = parseInt(req.params.id);
+  const updatedTask = req.body;
+  const index = tasks.findIndex((task) => task.id === taskId);
+  if (index !== -1) {
+    tasks[index] = {
+      id: taskId,
+      name: updatedTask.name,
+      description: updatedTask.description,
+      isDone: updatedTask.isDone,
+    };
+    res.json(tasks[index]);
+  } else {
+    res.status(404).json({ error: 'Task not found' });
+  }
+});
+
+// DELETE /tasks/:id: exclui uma tarefa existente.
+app.delete('/tasks/:id', (req, res) => {
+  const taskId = parseInt(req.params.id);
+  const index = tasks.findIndex((task) => task.id === taskId);
+  if (index !== -1) {
+    tasks.splice(index, 1);
+    res.sendStatus(204);
+  } else {
+    res.status(404).json({ error: 'Task not found' });
+  }
+});
+
+// Inicia o servidor na porta 3000
+app.listen(3000, () => {
+  console.log('Servidor iniciado na porta 3000');
 });
